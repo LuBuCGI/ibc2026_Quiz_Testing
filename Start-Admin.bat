af03a24ffa839bfb5423500e18012e550ef649c5
LuBuCGI <lukas.burs@cgi.com>
2026-08-20 13:07:55 +0200

    Add files via upload

---
+++ Start-Admin.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+set "ROOT=%~dp0"
+set "INDEX=%ROOT%de.html"
+
+rem Convert the local path to a file:// URL and open the admin view.
+for /f "usebackq delims=" %%I in (`powershell -NoLogo -NoProfile -Command "[uri]::new((Resolve-Path '%INDEX%')).AbsoluteUri + '?admin=1'"`) do set "URL=%%I"
+
+set "EDGE1=%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe"
+set "EDGE2=%ProgramFiles%\Microsoft\Edge\Application\msedge.exe"
+set "CHROME1=%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
+set "CHROME2=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
+
+if exist "%EDGE1%" (
+  start "" "%EDGE1%" --app="%URL%" --start-fullscreen --no-first-run --disable-translate --disable-pinch
+) else if exist "%EDGE2%" (
+  start "" "%EDGE2%" --app="%URL%" --start-fullscreen --no-first-run --disable-translate --disable-pinch
+) else if exist "%CHROME1%" (
+  start "" "%CHROME1%" --app="%URL%" --start-fullscreen --no-first-run --disable-translate --disable-pinch
+) else if exist "%CHROME2%" (
+  start "" "%CHROME2%" --app="%URL%" --start-fullscreen --no-first-run --disable-translate --disable-pinch
+) else (
+  start "" "%INDEX%?admin=1"
+)
+
+endlocal
